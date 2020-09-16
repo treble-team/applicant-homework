9c48e8240513e28150e50fa75a629bf686d6cff2
Teppo Pöntelin <teppo.pontelin@if.fi>
2020-09-16 14:33:25 +0300

    Added chart component

--- app/ClientApp/src/App.tsx
+++ app/ClientApp/src/App.tsx
@@ -1,14 +1,34 @@
 import React from "react";
 import "./App.css";
 import BarChart from "./BarChart";
+import ChartD3 from "./ChartD3";
+import data from "./corona-data-fi.json";
+// @ts-ignore
+import * as _ from "lodash";
 
 function App() {
+  const numberData = data.data.timeline
+    .sort((a, b) => (a.date < b.date ? -1 : a.date > b.date ? 1 : 0))
+    .map((day) => ({
+      Date: day.date,
+      Confirmed: day.confirmed,
+    }));
+
+  var content = (
+    <ChartD3
+      data={_.cloneDeep(numberData)}
+      title={"Confirmed Covid cases in Finland"}
+    />
+  );
+
   return (
     <div className="App">
       <header className="App-header"></header>
       <div id="testing">
         <BarChart></BarChart>
       </div>
+
+      {content}
     </div>
   );
 }
---
+++ app/ClientApp/src/ChartD3.jsx
@@ -0,0 +1,56 @@
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import PropTypes from "prop-types";
+import chart from "./chart.js";
+import * as _ from "lodash";
+
+class ChartD3 extends Component {
+  constructor(props) {
+    super(props);
+    this.ref = React.createRef();
+  }
+
+  componentDidMount() {
+    let el = this.ref.current;
+
+    chart.create(
+      el,
+      {
+        width: 960,
+        height: 500,
+      },
+      this.getChartState()
+    );
+  }
+
+  componentDidUpdate() {
+    chart.update(this.getChartState());
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    return !_.isEqual(nextProps.data, this.props.data);
+  }
+
+  getChartState() {
+    return {
+      data: this.props.data,
+      title: this.props.title,
+    };
+  }
+
+  componentWillUnmount() {
+    let el = ReactDOM.findDOMNode(this);
+    chart.cleanUp(el);
+  }
+
+  render() {
+    return <div className="chart" ref={this.ref}></div>;
+  }
+}
+
+ChartD3.propTypes = {
+  data: PropTypes.array,
+  title: PropTypes.string,
+};
+
+export default ChartD3;
